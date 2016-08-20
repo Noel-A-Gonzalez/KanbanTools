3af0036c3f8cf232b24febb46dfcff6d5760e055
natalia gomez <natalia.e.g@hotmail.com>
2016-08-20 18:35:00 -0300

    Hola Noel

--- public/javascripts/admin/reportes.js
+++ public/javascripts/admin/reportes.js
@@ -22,11 +22,11 @@ var reportes = function(){
     			var fechaFin = moment(fechaF).format('DD/MM/YYYY');
 			    $('#tblReport>tbody').append(
                     '<tr>'+
-	                    '<td>Tarea '+ num +'</td>'+
-	                    '<td>'+data[i].out_title+'</td>'+
-	                    '<td>'+fechaInicio+'</td>'+
-	                    '<td>'+fechaFin+'</td>'+
-	                    '<td>'+(data[i].out_cant_dias+1) +'</td>'+
+	                    '<td>Tarea '+ num + '</td>'+
+	                    '<td>'+ data[i].out_title + '</td>'+
+	                    '<td>'+ fechaInicio + '</td>'+
+	                    '<td>'+ fechaFin + '</td>'+
+	                    '<td>'+ data[i].out_cant_dias +'</td>'+
                     '</tr>');
                 $(".btn-save").hide();
                 $(".loading").hide();
@@ -38,96 +38,58 @@ var reportes = function(){
 	var initReport = function(){
 		$.get("../report/getDataReport")
 		.done(function(data){
-			var dataJSON = jQuery.parseJSON(data[0].sp_report_posit_ini_fin);
-			$('#container').highcharts({
-		        chart: {
-		            type: 'column'
-		        },
-		        title: {
-		            text: 'Tiempo de progreso de las tareas finalizadas'
-		        },
-		        subtitle: {
-		            text: 'Click en cada columna para ver mas información..'
-		        },
-		        xAxis: {
-		            type: 'category'
-		        },
-		        yAxis: {
-		            title: {
-		                text: 'Total de días desde su inicio a su fin'
-		            }
+			$.get("../report/getDataReport2")
+			.done(function(data2){
+				var dataJSON = jQuery.parseJSON(data[0].sp_report_posit_ini_fin);
+				var dataJSON2 = jQuery.parseJSON(data2[0].sp_report_posit_ini_fin_2);
+				$('#container').highcharts({
+			        chart: {
+			            type: 'column'
+			        },
+			        title: {
+			            text: 'Tiempo de progreso de las tareas finalizadas'
+			        },
+			        subtitle: {
+			            text: 'Click en cada columna para ver mas información..'
+			        },
+			        xAxis: {
+			            type: 'category'
+			        },
+			        yAxis: {
+			            title: {
+			                text: 'Total de días desde su inicio a su fin'
+			            }
 
-		        },
-		        legend: {
-		            enabled: true
-		        },
-		        plotOptions: {
-		            series: {
-		                borderWidth: 0,
-		                dataLabels: {
-		                    enabled: true,
-		                    format: '{point.y:.0f}'
-		                }
-		            }
-		        },
+			        },
+			        legend: {
+			            enabled: true
+			        },
+			        plotOptions: {
+			            series: {
+			                borderWidth: 0,
+			                dataLabels: {
+			                    enabled: true,
+			                    format: '{point.y:.0f}'
+			                }
+			            }
+			        },
 
-		        tooltip: {
-		            headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-		            pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.0f}</b> dias<br/>'
-		        },
+			        tooltip: {
+			            headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+			            pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.0f}</b> dias<br/>'
+			        },
 
-		        series: [{
-		            name: 'Tareas',
-		            colorByPoint: true,
-		            data: dataJSON
-		        }],
-		        drilldown: {
-		            series: [{
-		                name: '1',
-		                id: '1',
-		                data: [
-		                    [
-		                        'TO-DO',
-		                        24
-		                    ],
-		                    [
-		                        'DOING',
-		                        17
-		                    ],
-		                    [
-		                        'VERIFY',
-		                        5
-		                    ],
-		                    [
-		                        'DONE',
-		                        6
-		                    ]
-		                ]
-		            }, {
-		                name: '3',
-		                id: '3',
-		                data: [
-		                    [
-		                        'TO-DO',
-		                        5
-		                    ],
-		                    [
-		                        'DOING',
-		                        4
-		                    ],
-		                    [
-		                        'VERIFY',
-		                        3
-		                    ],
-		                    [
-		                        'DONE',
-		                        7
-		                    ]
-		                ]
-		            }]
-		        }
-		    });
-
+			        series: [{
+			            name: 'Posit',
+			            colorByPoint: true,
+			            data: dataJSON
+			        }],
+			        drilldown: {
+			            series: dataJSON2
+			        }
+			    });
+			})
+			
 		});	 
 	}
 
--- routes/report.js
+++ routes/report.js
@@ -34,4 +34,19 @@ report.get("/getDataReport", function(req, res) {
 	});
 });
 
+report.get("/getDataReport2", function(req, res) {
+	var jsonStringify, data;
+	client.query('SELECT * FROM app_kanban.sp_report_posit_ini_fin_2()', function(err, result){
+		if (err) {
+			console.log(err);
+			res.status(500);
+			return;
+		}else{
+			jsonStringify = JSON.stringify(result.rows);
+	      	data = JSON.parse(jsonStringify)
+	     	res.jsonp(data);
+		}
+	});
+});
+
 module.exports = report;
